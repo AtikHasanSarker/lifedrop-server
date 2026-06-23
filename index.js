837b7bc5e200513561f2dc9604fbb4d25b580345
AtikHasanSarker <atiksagor24@gmail.com>
2026-06-24 04:08:52 +0600

    Connected to mongodb

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
-
+const uri = process.env.MONGODB_URI;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB client and collection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("lifedrop");
+    const donationRequestCollection = db.collection("donationRequest");
+    console.log("Successfully Connected to MongoDB: lifedrop");
+
+    
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running for tutors!");
