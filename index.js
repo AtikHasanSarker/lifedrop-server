d0c95e9ac3db90f0f4f649312d7213fd588c3b05
AtikHasanSarker <atiksagor24@gmail.com>
2026-06-29 11:12:41 +0600

    Created api for saving payment data into MONGODB and get the data

--- index.js
+++ index.js
@@ -27,12 +27,60 @@ async function run() {
   try {
     await client.connect();
     const db = client.db("lifedrop");
-    const donationRequestCollection = db.collection("donation-requests");
     console.log("Successfully Connected to MongoDB: lifedrop");
+    const donationRequestCollection = db.collection("donation-requests");
+    const usersCollection = db.collection("user");
+    const fundingCollection = db.collection("funding");
 
     app.post("/donation-requests", async (req, res) => {
       const requestData = req.body;
       const result = await donationRequestCollection.insertOne(requestData);
+      res.json(result);
+    });
+
+    app.post("/funding", async (req, res) => {
+      try {
+        const {
+          userId,
+          userName,
+          UserEmail,
+          amount,
+          currency,
+          paymentIntent,
+          checkoutSessionId,
+          paymentStatus,
+          transactionDate,
+        } = req.body;
+
+        const funding = {
+          userId,
+          userName,
+          UserEmail,
+          amount,
+          currency,
+          paymentIntent,
+          checkoutSessionId,
+          paymentStatus,
+          transactionDate,
+          createdAt: new Date(),
+        };
+
+        const result = await fundingCollection.insertOne(funding);
+        return res.status(200).json({
+          success: true,
+          message: "Funding saved successfully",
+          result
+        });
+      } catch (error) {
+        return res.status(500).json({
+          success: false,
+          error: error.message || "Failed to save funding."
+        });
+      }
+    });
+
+    app.get("/funding", async (req, res) => {
+      const result = await fundingCollection.find().toArray();
       res.json(result);
     });
 
@@ -41,13 +89,13 @@ async function run() {
       res.json(result);
     });
 
-    
+
     app.get("/donation-requests/:id", async (req, res) => {
       const id = req.params.id;
       const result = await donationRequestCollection.findOne({ _id: new ObjectId(id) });
       res.json(result);
     });
-    
+
     app.patch("/donation-requests/:id", async (req, res) => {
       const id = req.params.id;
       const result = await donationRequestCollection.updateOne(
@@ -56,7 +104,22 @@ async function run() {
       );
       res.json(result);
     });
-    
+
+    app.patch("/public-donation-requests/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await donationRequestCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: req.body },
+      );
+      res.json(result);
+    });
+
+    app.delete("/donation-requests/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await donationRequestCollection.deleteOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
+
     app.get("/my-donation-requests/:userId", async (req, res) => {
       const { userId } = req.params;
       const query = {
@@ -67,7 +130,37 @@ async function run() {
       res.json(myRequests);
     });
 
-    
+
+
+    // Search donors by blood group, district, upazila
+    app.get("/search-donors", async (req, res) => {
+      const { bloodGroup, district, upazila } = req.query;
+
+      const query = {
+        bloodGroup,
+        district,
+        upazila,
+        role: "donor",
+      };
+      const donors = await usersCollection.find(query).toArray();
+      res.json(donors);
+    });
+
+    //for admin
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.json(result);
+    });
+
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await usersCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: req.body },
+      );
+      res.json(result);
+    });
+
   } finally {
     // await client.close();
   }
