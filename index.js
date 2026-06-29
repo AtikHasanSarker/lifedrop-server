26bc9ed1e9fb5618eb3e350e3d96a3897c381b55
AtikHasanSarker <atiksagor24@gmail.com>
2026-06-29 17:11:00 +0600

    Added jwt to secure backend

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const uri = process.env.MONGODB_URI;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB client and collection
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,6 +21,28 @@ const client = new MongoClient(uri, {
 });
 
 
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
+);
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log("payload from jwt:", payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
 
 async function run() {
   try {
